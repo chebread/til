3e3b7ed73f468da257ee748b6c8f7e81e9218d02
Cha Haneum <developer_haneum_11@icloud.com>
2023-02-27 17:47:01 +0900

    📝 init

--- grid/src/Example.tsx
+++ grid/src/Example.tsx
@@ -4,22 +4,49 @@ const Example = () => {
   return (
     <Container>
       <Item>A</Item>
-      <Item>A</Item>
+      <Item>B</Item>
+      <Item>C</Item>
+      <Item>D</Item>
+      <Item>E</Item>
+      <Item>F</Item>
+      <Item>G</Item>
+      <Item>H</Item>
+      <Item>I</Item>
+      <Item>J</Item>
     </Container>
   );
 };
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, auto));
-  // 400px < x < 600px에서 Container가 x의 크기를 가지면 자동적으로 아이템 늘어나게 된다. (그리드의 기본 설정)
-  // 그치만 600 초과시에는 400px에서 600미만까지 그냥 커지기만 하지 또 600보다 커지지는 않는다. (아이템이 없으면)
-  // 그래서 이럴 경우는 auto-fit을 쓴다!
+
+  grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: minmax(100px, auto);
+  gap: 1em;
 `;
 const Item = styled.div`
   background-color: seagreen;
   border: 5px solid black;
   box-sizing: border-box;
+
+  &:nth-child(1) {
+    grid-column: 1 / 3;
+    grid-row: 1 / 4;
+  }
+  &:nth-child(4) {
+    grid-column: 2 / 3; // 이렇게 되면 겹치게 될 수도 있다. 겹칠때에는 항상 모든 지정 번호를 다 써주어야 한다.
+    // 또한 겹치면 z-index로 올릴 수도 있다!
+    grid-row: 1 / 2;
+    z-index: 1;
+  }
+  &:nth-child(2) {
+    grid-column: 2 / 3; // 이렇게 시작 부터 한칸만 간다면 끝깞은 생략가능한데, 겹치기를 사용한다면 모두 적어주는게 좋다
+    grid-row: 2 / 3; // 1 부터 2칸 갑니다 라고 표현하려면 1 / span 2 라고 해도 된다
+  }
+  &:last-child {
+    grid-column: 2 / 4;
+    // row는 기본값
+  }
 `;
 
 export default Example;
